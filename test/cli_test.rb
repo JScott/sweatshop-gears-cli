7410b97d218bd17f0f1876244f73c11d2b83d473
JScott <jvscott@gmail.com>
2015-05-28 08:17:39 -0700

    Red tests for init

--- test/cli_test.rb
+++ test/cli_test.rb
@@ -2,8 +2,18 @@ require 'teststrap'
 require 'robot_sweatshop/tears'
 
 context 'the CLI tool' do
+  setup { File.expand_path "#{__dir__}/../bin/sweatshop-tears" }
   context 'with no arguments' do
-    setup { `#{__dir__}/../bin/sweatshop-tears` }
+    setup { `#{topic}` }
+    asserts('doing something') { not topic.empty? }
+  end
+  context 'with init' do
+    setup { `#{topic} init` }
     asserts('doing something') { not topic.empty? }
   end
 end
+
+# init
+# remove
+# list
+# show
