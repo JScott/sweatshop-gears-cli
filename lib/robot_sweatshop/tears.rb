807474268c1a20af7ffbfde440c94ad49aa6c05e
JScott <jvscott@gmail.com>
2015-05-29 15:36:16 -0700

    Started working toward green tests, got interested in comparing rspec stuff so breaking here

--- lib/robot_sweatshop/tears.rb
+++ lib/robot_sweatshop/tears.rb
@@ -1,7 +1,32 @@
+require 'contracts'
+require 'git'
+require 'terminal-announce'
+require 'robot_sweatshop/config'
+
 module Tears
+  include Contracts
+
+  #     repo = Git.open download_path
+  #     puts repo.pull
+
+  Contract None => nil
+  def self.clone_packages
+    Git.clone git_repo, 'sweatshop-tears-packages', path: download_path
+  end
+
+  Contract None => nil
+  def self.download
+    return if Dir.exist? download_path
+    Announce.info "Downloading packages from #{git_repo}"
+    clone_packages
+  end
+
+  Contract None => String
   def self.git_repo
     'git@github.com:JScott/sweatshop-tears-packages.git'
   end
+
+  Contract None => String
   def self.download_path
     '/opt/tears'
   end
--- sweatshop-tears.gemspec
+++ sweatshop-tears.gemspec
@@ -19,7 +19,8 @@ Gem::Specification.new do |gem|
   gem.add_runtime_dependency 'commander'
   gem.add_runtime_dependency 'robot_sweatshop'
   gem.add_runtime_dependency 'contracts'
-  gem.add_runtime_dependency 'cli-announce'
+  gem.add_runtime_dependency 'terminal-announce'
+  gem.add_runtime_dependency 'git'
 
   gem.add_development_dependency 'rake'
   gem.add_development_dependency 'riot'
