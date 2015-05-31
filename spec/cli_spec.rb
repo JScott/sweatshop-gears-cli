ebcb97bb32fdcf56a38d950bbcea38b3a961f866
JScott <jvscott@gmail.com>
2015-05-31 16:43:20 -0700

    Caught up Riot. Looking to be better behaved that RSpec

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,16 +1,17 @@
 require 'fileutils'
 
+SCRIPT_PATH = File.expand_path "#{__dir__}/../bin/sweatshop-tears"
+PACKAGE_PATH = File.expand_path '.test_download_path'
+
 RSpec.describe 'sweatshop-tears' do
-  let(:script_path) { File.expand_path "#{__dir__}/../bin/sweatshop-tears" }
-  let(:package_path) { File.expand_path '.test_download_path' }
   context 'with no command' do
-    subject { `#{script_path}` }
+    subject { `#{SCRIPT_PATH}` }
     it { is_expected.to_not be_empty }
   end
   context 'init' do
-    subject { `#{script_path} init --path #{package_path}` }
-    after { FileUtils.rm_rf '.test' }
+    subject { a = `#{SCRIPT_PATH} init --path #{PACKAGE_PATH}`; p Dir[PACKAGE_PATH]; a }
+    after(:all) { FileUtils.rm_rf PACKAGE_PATH; p Dir[PACKAGE_PATH] }
     it { is_expected.to_not be_empty }
-    it { expect(Dir).to exist(package_path) }
+    it { expect(Dir).to exist(PACKAGE_PATH) }
   end
 end
--- test/cli_test.rb
+++ test/cli_test.rb
@@ -1,19 +1,19 @@
 require 'teststrap'
 
+SCRIPT_PATH = File.expand_path "#{__dir__}/../bin/sweatshop-tears"
+PACKAGE_PATH = File.expand_path '.test_download_path'
+
 context 'sweatshop-tears' do
   setup { File.expand_path "#{__dir__}/../bin/sweatshop-tears" }
   context '' do
-    setup { `#{topic}` }
+    setup { `#{SCRIPT_PATH}` }
     denies_topic('the output').empty
   end
-
   context 'init' do
-    setup do
-      #
-      `#{topic} init`
-    end
+    setup { `#{SCRIPT_PATH} init --path #{PACKAGE_PATH}` }
     denies_topic('the output').empty
-    asserts('packages are downloaded') { Dir.exist? Tears.download_path }
+    asserts('packages are downloaded') { Dir.exist? PACKAGE_PATH }
+    teardown { FileUtils.rm_rf PACKAGE_PATH }
   end
 end
 
