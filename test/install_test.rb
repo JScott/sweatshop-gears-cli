83501cc203ac723c37aec9b0f8d4b5c3f084a5bc
JScott <jvscott@gmail.com>
2015-06-16 20:54:34 -0700

    Less redundancy in install_test.rb

--- test/install_test.rb
+++ test/install_test.rb
@@ -1,19 +1,16 @@
 require_relative 'teststrap'
 
 context 'sweatshop-gears install' do
-  setup { clean_up_paths }
-  context 'binary' do
-    setup { `sweatshop-gears install git-sync --path #{PACKAGES_PATH}` }
-    denies_topic('the output').empty
-    asserts('packages are downloaded') { File.exist? "#{PACKAGES_PATH}/packages/git-sync" }
-    asserts('packages binary is linked') { File.exist? "#{SWEATSHOP_SCRIPTS_PATH}/git-sync" }
-    # asserts('dependencies are installed') { ??? }
+  setup do
+    clean_up_paths
+    %w(git-sync big-green-button).each do |package|
+      `sweatshop-gears install #{package} --path #{PACKAGES_PATH}`
+    end
   end
-  context 'service' do
-    setup { `sweatshop-gears install big-green-button --path #{PACKAGES_PATH}` }
-    denies_topic('the output').empty
-    asserts('packages are downloaded') { File.exist? "#{PACKAGES_PATH}/packages/big-green-button" }
-    asserts('loads services into Eye') { not `eye info | grep big-green-button`.empty? }
-    # asserts('dependencies are installed') { ??? }
-  end
+  denies_topic('the output').empty
+  asserts('packages are downloaded') { File.exist? "#{PACKAGES_PATH}/packages/git-sync" }
+  asserts('binaries are symlinked') { File.exist? "#{SWEATSHOP_SCRIPTS_PATH}/git-sync" }
+  denies('services are symlinked') { File.exist? "#{SWEATSHOP_SCRIPTS_PATH}/big-green-button" }
+  asserts('services are loaded into Eye') { not `eye info | grep big-green-button`.empty? }
+  # asserts('dependencies are installed') { ??? }
 end
