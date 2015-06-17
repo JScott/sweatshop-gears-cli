f52730d45dfc53069506c17ecd67810efe512dfc
JScott <jvscott@gmail.com>
2015-06-16 20:48:29 -0700

    Fixing up leftovers from test framework

--- test/install_test.rb
+++ test/install_test.rb
@@ -1,18 +1,12 @@
 require_relative 'teststrap'
 
 context 'sweatshop-gears install' do
-  # setup { clean_up_paths }
+  setup { clean_up_paths }
   context 'binary' do
     setup { `sweatshop-gears install git-sync --path #{PACKAGES_PATH}` }
     denies_topic('the output').empty
     asserts('packages are downloaded') { File.exist? "#{PACKAGES_PATH}/packages/git-sync" }
-    asserts('packages binary is linked') do
-      file = "#{SWEATSHOP_SCRIPTS_PATH}/git-sync"
-      path = Dir["#{SWEATSHOP_SCRIPTS_PATH}/*"]
-      p file, path, path.include?(file), File.exist?(file)
-      File.exist?(file)
-    end
-    # asserts('packages binary is linked') { Dir["#{SWEATSHOP_SCRIPTS_PATH}/*"].each { |x| p File.exist? x }; File.exist? "#{SWEATSHOP_SCRIPTS_PATH}/git-sync" }
+    asserts('packages binary is linked') { File.exist? "#{SWEATSHOP_SCRIPTS_PATH}/git-sync" }
     # asserts('dependencies are installed') { ??? }
   end
   context 'service' do
--- test/teststrap.rb
+++ test/teststrap.rb
@@ -10,5 +10,5 @@ PACKAGES_PATH = '.test_download_path'
 SWEATSHOP_SCRIPTS_PATH = File.expand_path '.robot_sweatshop/scripts'
 
 def clean_up_paths
-  FileUtils.rm_rf [PACKAGES_PATH, '.robot_sweatshop']
+  FileUtils.rm_rf PACKAGES_PATH
 end
