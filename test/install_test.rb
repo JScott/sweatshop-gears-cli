9236ee0ed6cfea56985cf8cc68c3162acc6953aa
JScott <jvscott@gmail.com>
2015-06-17 17:44:02 -0700

    Red tests for making sure services don't overwrite each other

--- test/install_test.rb
+++ test/install_test.rb
@@ -3,7 +3,7 @@ require_relative 'teststrap'
 context 'sweatshop-gears install' do
   setup do
     clean_up_paths
-    %w(git-sync big-green-button).each do |package|
+    %w(git-sync big-green-button reporter).each do |package|
       `sweatshop-gears install #{package} --path #{PACKAGES_PATH}`
     end
   end
@@ -11,6 +11,6 @@ context 'sweatshop-gears install' do
   asserts('packages are downloaded') { File.exist? "#{PACKAGES_PATH}/git-sync" }
   asserts('binaries are symlinked') { File.exist? "#{SWEATSHOP_SCRIPTS_PATH}/git-sync" }
   denies('services are symlinked') { File.exist? "#{SWEATSHOP_SCRIPTS_PATH}/big-green-button" }
-  asserts('services are loaded into Eye') { not `eye info | grep big-green-button`.empty? }
+  asserts('all services are loaded into Eye') { `eye info | grep --extended-regexp 'big-green-button|reporter'`.lines.count == 2 }
   # asserts('dependencies are installed') { ??? }
 end
