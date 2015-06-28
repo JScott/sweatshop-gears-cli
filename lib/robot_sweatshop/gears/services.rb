af1697ae453e056cf5a34e9b026458be360fa410
JScott <jvscott@gmail.com>
2015-06-28 09:37:49 -0700

    Eye and Rack working together in a weird union

--- lib/robot_sweatshop/gears/services.rb
+++ lib/robot_sweatshop/gears/services.rb
@@ -7,12 +7,12 @@ module Gears
   module Services
     def self.expose(from_path:)
       service = File.basename from_path
+      generate_reverse_proxy from_path
       dynamically_load_eye from_path
       Announce.success "Loaded #{service} into Eye"
     end
 
     def self.write_and_load(eye_config)
-      eye_file = '/tmp/.sweatshop_gears.eye'
       File.write eye_file, eye_config
       puts `eye load #{eye_file}`
       puts `eye stop sweatshop_gears:services`
@@ -28,12 +28,32 @@ module Gears
       File.expand_path "#{install_path}/.."
     end
 
+    def self.eye_file
+      '/tmp/.sweatshop_gears.eye'
+    end
+
+    def self.proxy_file
+      '/tmp/.sweatshop_gears.ru'
+    end
+
     def self.service_hub_path
       "#{__dir__}/../../../bin/sweatshop-gears-service-hub"
     end
 
     def self.first_port
       34871
+    end
+
+    def self.generate_reverse_proxy(install_path)
+      services_path = parent_path_of install_path
+      context = {
+        proxy_file: proxy_file,
+        services: installed_in(services_path),
+        first_port: first_port
+      }
+      input = File.read "#{__dir__}/templates/proxy_config.ru.eruby"
+      eruby = Erubis::Eruby.new input
+      File.write proxy_file, eruby.result(context)
     end
 
     def self.dynamically_load_eye(install_path)
