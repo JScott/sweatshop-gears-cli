0d69b2b2f3fbe2e296ee31fa9464788d7a99e5d8
JScott <jvscott@gmail.com>
2015-06-29 10:06:54 -0700

    I was making things way more complicated than need be

--- lib/robot_sweatshop/gears/service_hub.rb
+++
@@ -1,8 +0,0 @@
-# Helper methods for the Service Hub Sinatra server
-module Gears
-  module ServiceHub
-    def frontpage_template
-      File.read "#{__dir__}/templates/frontpage.html.eruby"
-    end
-  end
-end
--- lib/robot_sweatshop/gears/services.rb
+++ lib/robot_sweatshop/gears/services.rb
@@ -7,7 +7,6 @@ module Gears
   module Services
     def self.expose(from_path:)
       services_path = File.expand_path "#{from_path}/.."
-      generate_reverse_proxy for_services: installed_in(services_path)
       dynamically_load_eye_from services_path
       service = File.basename from_path
       Announce.success "Loaded #{service} into Eye"
@@ -16,9 +15,7 @@ module Gears
     def self.write_and_load(eye_config)
       File.write eye_file, eye_config
       puts `eye load #{eye_file}`
-      puts `eye stop sweatshop_gears:services`
-      puts `eye restart sweatshop_gears:service_hub`
-      puts `eye restart sweatshop_gears:service_proxy`
+      puts `eye stop sweatshop_gears`
     end
 
     def self.installed_in(path)
@@ -30,35 +27,15 @@ module Gears
       '/tmp/.sweatshop_gears.eye'
     end
 
-    def self.proxy_file
-      '/tmp/.sweatshop_gears.ru'
-    end
-
-    def self.service_hub_path
-      "#{__dir__}/../../../bin/sweatshop-gears-service-hub"
-    end
-
     def self.first_port
       34871
-    end
-
-    def self.generate_reverse_proxy(for_services:)
-      context = {
-        services: for_services,
-        first_port: first_port
-      }
-      input = File.read "#{__dir__}/templates/proxy_config.ru.eruby"
-      eruby = Erubis::Eruby.new input
-      File.write proxy_file, eruby.result(context)
     end
 
     def self.dynamically_load_eye_from(services_path)
       context = {
         config: Gears::Metadata.sweatshop_config,
         services: installed_in(services_path),
-        services_path: File.expand_path(services_path),
-        service_hub_bin: File.expand_path(service_hub_path),
-        proxy_file: proxy_file,
+        services_path: services_path,
         first_port: first_port
       }
       input = File.read "#{__dir__}/templates/sweatshop_gears.eye.eruby"
